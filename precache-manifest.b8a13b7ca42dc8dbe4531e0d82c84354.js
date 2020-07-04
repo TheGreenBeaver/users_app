8cb040175581a2491d4686dfd89a426e5c6afd00
TheGreenBeaver <leonkhvatskin@gmail.com>
2020-07-05 00:06:02 +0300

    Updates

---
+++ precache-manifest.b8a13b7ca42dc8dbe4531e0d82c84354.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ac229f4a4af78ba6ea5049761f67be5",
+    "url": "/users_app/index.html"
+  },
+  {
+    "revision": "b19c04163c50fe901418",
+    "url": "/users_app/static/css/main.d4e84cf9.chunk.css"
+  },
+  {
+    "revision": "2b32ff6e0e40416cbea9",
+    "url": "/users_app/static/js/2.3944675a.chunk.js"
+  },
+  {
+    "revision": "ffa35a34964860fdc97ed79ca9abd27c",
+    "url": "/users_app/static/js/2.3944675a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b19c04163c50fe901418",
+    "url": "/users_app/static/js/main.47fe9815.chunk.js"
+  },
+  {
+    "revision": "f60839bd5b957a2cf726",
+    "url": "/users_app/static/js/runtime-main.6af9cc92.js"
+  },
+  {
+    "revision": "c7daaf195b882fd26790f7fa5222b105",
+    "url": "/users_app/static/media/backToList.c7daaf19.svg"
+  },
+  {
+    "revision": "2681113b396dd7f07fd2b3ed7139658a",
+    "url": "/users_app/static/media/backToListHvr.2681113b.svg"
+  },
+  {
+    "revision": "070803ed50811a62e49800244cca7858",
+    "url": "/users_app/static/media/nextUsr.070803ed.svg"
+  },
+  {
+    "revision": "2cd3d7f95f6c9e515504d830b0a8e806",
+    "url": "/users_app/static/media/nextUsrHvr.2cd3d7f9.svg"
+  },
+  {
+    "revision": "55aa1eb2e7b37c2fb1afe9b9a805621e",
+    "url": "/users_app/static/media/prevUsr.55aa1eb2.svg"
+  },
+  {
+    "revision": "6d516444836cf5d7c09c65e1b5651b20",
+    "url": "/users_app/static/media/prevUsrHvr.6d516444.svg"
+  },
+  {
+    "revision": "3d591ffe15b019dba53c34d39295fb70",
+    "url": "/users_app/static/media/tick.3d591ffe.svg"
+  }
+]);
